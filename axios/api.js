fd64dd025044289ed34acd8e161ce4f8fdcbd0f3
krishna kushwaha <krishnakushwaha2248@gmail.com>
2026-08-29 19:06:56 +0530

    added the unauth token remove functionality

--- axios/api.js
+++ axios/api.js
@@ -19,8 +19,21 @@ api.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
-  }
+  },
 );
 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      if (typeof window !== "undefined") {
+        localStorage.removeItem("token");
+        window.location.href = "/signin";
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default api;
